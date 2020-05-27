9828a9ea5bc05e93df5ed5b0b4e138bf288ffb7d
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-05-27 17:51:11 -0400

    added more options to the interface and made it less verbose

--- 1420_psd.py
+++ 1420_psd.py
@@ -14,7 +14,7 @@ def main(argv):
     if(not len(argv)):
         sys.exit("Usage:\n1420_psd.py -i <integration time(s)>")
     try:
-        opts, args = getopt.getopt(argv, "hi:", ["integrate=", "background"])
+        opts, args = getopt.getopt(argv, "hi:", ["integrate=", "background", "doplot", "verbose"])
     except getopt.GetoptError:
         print('Usage:\n1420_psd.py -i <integration time (s)> (--background)')
         sys.exit(2)
@@ -30,10 +30,22 @@ def main(argv):
                     sys.exit("Integration time must be a positive integer number of seconds")
             else:
                 sys.exit("Error: argument must be an integer number of seconds")
-        if opt == '--background':
-            filesuffix = '_background'
-        else:
-            filesuffix = ""
+
+    if '--background' in opts:
+        filesuffix = '_background'
+    else:
+        filesuffix = ""
+
+    if "--doplot" in opts:
+        doplot = True
+    else:
+        doplot = False
+
+    if "--verbose" in opts:
+        verbose = True
+    else:
+        verbose = False
+
 
 
 
@@ -73,7 +85,8 @@ def main(argv):
         n = len(samples)
         power.append(ps[idx]/n)
 
-    print('Averaging samples...')
+    if verbose:
+        print('Averaging samples...')
     avgpower = []
     for ii in range(numsamples):
         avg_i = 0
@@ -81,26 +94,34 @@ def main(argv):
             avg_i += power[jj][ii]
         avgpower.append(avg_i/passes)
 
-    rvel = (constants.c*((hi_restfreq - u.Quantity(frequency, u.MHz)/hi_restfreq)).to(u.km/u.s).value
+    rvel = (constants.c*((hi_restfreq - u.Quantity(frequency, u.MHz))/hi_restfreq)).to(u.km/u.s).value
 
-    print('Writing output files...')
+    now = str(datetime.datetime.now().strftime("%y%m%d_%H%M%S"))
+
+    if verbose:
+        print('Writing output files...')
     # write plot
 
-    fig, ax = plt.subplots()
-    ax.plot(rvel, 10*np.log10(avgpower), 'b-')
-    ax.tick_params(labelsize=6)
-    ax.get_xaxis().get_major_formatter().set_useOffset(False)
-    ax.set_xlabel('Relative Velocity (km/s)')
-    ax.set_ylabel('Measured Power (dB)')
-    now = str(datetime.datetime.now().strftime("%y%m%d_%H%M%S"))
-    try:
-        fig.savefig('psd_' + now + '.pdf')
-    except:
-        print("FAILED savefig")
+    if doplot:
+        fig, ax = plt.subplots()
+        ax.plot(rvel, 10*np.log10(avgpower), 'b-')
+        ax.tick_params(labelsize=6)
+        ax.get_xaxis().get_major_formatter().set_useOffset(False)
+        ax.set_xlabel('Relative Velocity (km/s)')
+        ax.set_ylabel('Measured Power (dB)')
+        try:
+            fig.savefig('psd_' + now + '.pdf')
+        except:
+            print("FAILED savefig")
 
     # write csv
 
     filename = 'psd_' + now + "_t=" + str(int_time) + filesuffix + '.csv'
+    filename = f"psd_{now}_tint{int_time}s{filesuffix}.csv"
+    if verbose:
+        print(filename)
+    assert "'" not in filename
+    assert '"' not in filename
 
     with open(filename, 'w') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quotechar='|',
@@ -109,10 +130,6 @@ def main(argv):
             writer.writerow([frequency[ii], 10*np.log10(avgpower[ii]),
                              rvel[ii]])
 
-    plt.plot(rvel, 10*np.log10(avgpower))
-    plt.xlabel('Relative Velocity (km/s)')
-    plt.ylabel('Measured Power (dB)')
-    plt.show()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
